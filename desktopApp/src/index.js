9a4b946235d8b62c8e0a5cf975f60b54fe63b3d4
Maxx <maxhiga.hawaii@gmail.com>
2021-08-19 18:28:05 -1000

    added code to make webcontents.send work

--- desktopApp/src/index.js
+++ desktopApp/src/index.js
@@ -1,8 +1,11 @@
-const { app, BrowserWindow } = require('electron');
-const path = require('path');
+const { app, BrowserWindow } = require("electron");
+const os = require("os-utils");
+const electron = require("electron");
+const path = require("path");
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
+if (require("electron-squirrel-startup")) {
+  // eslint-disable-line global-require
   app.quit();
 }
 
@@ -11,30 +14,46 @@ const createWindow = () => {
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
   });
 
   // and load the index.html of the app.
-  mainWindow.loadFile(path.join(__dirname, 'index.html'));
+  mainWindow.loadFile(path.join(__dirname, "index.html"));
 
   // Open the DevTools.
   mainWindow.webContents.openDevTools();
+
+  os.cpuUsage((v) => {
+    console.log("cpu usage %" + v * 100);
+    mainWindow.webContents.on("did-finish-load", () => {
+      mainWindow.webContents.send("cpu", v * 100);
+      mainWindow.webContents.send("mem", os.freememPercentage() * 100);
+      mainWindow.webContents.send("total-mem", os.totalmem() / 1024);
+    });
+    console.log("mem usage %" + os.freememPercentage() * 100);
+
+    console.log("total mem GB" + os.totalmem() / 1024);
+  });
 };
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+app.on("ready", createWindow);
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
     app.quit();
   }
 });
 
-app.on('activate', () => {
+app.on("activate", () => {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (BrowserWindow.getAllWindows().length === 0) {
